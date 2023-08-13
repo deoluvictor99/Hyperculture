dcfa4931a21bbb2caaf683b27d81c1991445a045
Adeniji Victor Adeolu <adenijivictor99.va@gmail.com>
2023-08-13 23:09:13 +0100

    first push to Github

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,32 +4,33 @@ export const theme = {
   extend: {
     colors: {
       "primary-colors-primary-500": "#2F6B5C",
-      "Secondary-colors/Secondary-600": "#779D42",
+      "Secondary-colors/Secondary-600": "#329530",
       "Greys/Grey-600": "#52525B",
     },
     fontFamily: {
       Nunito: ["Nunito", "sans-serif"],
       Comfortaa: ["Comfortaa", "cursive"],
+      Raleway: ["Raleway", "sans - serif"],
     },
     lineHeight: {
       "extra-loose": "2.5",
       standard: "1.2",
     },
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
 
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
 
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
 
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
 };

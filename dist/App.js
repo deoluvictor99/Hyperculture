c94f9b66d4ab42322f1dd7a83f1b73067fa851fd
Adeniji Victor Adeolu <adenijivictor99.va@gmail.com>
2023-08-15 17:49:35 +0100

    adjusted font size for mobile

--- dist/App.js
+++ dist/App.js
@@ -1,10 +1,8 @@
 const menu = (e) => {
     const ul = document.querySelector('ul');
-    const button = document.querySelector('button');
-    console.log(button);
 
-    e.name === "menu-outline"? 
-        (e.name = "close-outline",
+    e.name === "menu-outline"
+        ? ((e.name = "close-outline"),
             ul.classList.remove("hidden"),
             ul.classList.add("sm:absolute"),
             ul.classList.add("sm:top-[82.5px]"),
@@ -13,10 +11,9 @@ const menu = (e) => {
             ul.classList.add("sm:bg-white"),
             ul.classList.add("sm:px-[1.5rem]"),
             ul.classList.add("sm:w-[100%]")
-            
-        ):
-        (e.name ="menu-outline", 
-            ul.classList.add("hidden")
         )
+        : ((e.name = "menu-outline"),
+            ul.classList.add("hidden")
+        );
 
 }

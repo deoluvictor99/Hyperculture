c9dc1165d1104958b0b196238873ae091c6ef0d5
Adeniji Victor Adeolu <adenijivictor99.va@gmail.com>
2023-08-15 18:57:40 +0100

    added hover effects and scroll-to function

--- dist/App.js
+++ dist/App.js
@@ -1,8 +1,11 @@
-const menu = (e) => {
-    const ul = document.querySelector('ul');
+const ul = document.querySelector('ul');
+const allNavLinks = document.querySelectorAll('li');
+const ionIcon = document.querySelector('ion-icon');
 
-    e.name === "menu-outline"
-        ? ((e.name = "close-outline"),
+const menu = (e) => {
+
+    e.name === "menu-outline"?
+            ((e.name = "close-outline"),
             ul.classList.remove("hidden"),
             ul.classList.add("sm:absolute"),
             ul.classList.add("sm:top-[82.5px]"),
@@ -11,9 +14,36 @@ const menu = (e) => {
             ul.classList.add("sm:bg-white"),
             ul.classList.add("sm:px-[1.5rem]"),
             ul.classList.add("sm:w-[100%]")
-        )
-        : ((e.name = "menu-outline"),
+        ):
+            ((e.name = "menu-outline"),
             ul.classList.add("hidden")
         );
 
 }
+
+
+function scrollToSection(event) {
+  // hide dropdown on smaller screens after clicking
+    ul.classList.add("hidden");
+
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    let targetPosition = targetElement.getBoundingClientRect().top;
+    const headerHeight = document.querySelector("nav").offsetHeight;
+
+    const finalScrollPosition = targetPosition + window.scrollY - headerHeight;
+        window.scroll({
+        top: finalScrollPosition,
+        behavior: "smooth",
+        });
+}
+
+    allNavLinks.forEach((link) => {
+        
+        link.addEventListener("click", scrollToSection);
+        link.addEventListener("click", () => {
+            ionIcon.setAttribute('name', 'menu-outline')
+        })
+        ul.classList.add("hidden");
+    });
